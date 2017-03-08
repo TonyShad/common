6b91610341084c8495a8d39c1758638a71fc8a88
Maxim <McSim1993@gmail.com>
2017-03-08 23:38:21 +0300

    Починил зависимости

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 /**
  * Created by mcsim-gr on 08.03.17.
  */
+const fs = require('fs');
+const path = require('path');
+const data = {};
 
+const dataFiles = fs.readdirSync('./data');
+
+for (let file of dataFiles) {
+	data[path.basename(file, path.extname(file))] = require('./data/'+file);
+}
+
+console.log(data);
 
 module.exports = {
     Error: require('./lib/error'),
-    ioClient: require('./lib/client')
+    ioClient: require('./lib/client'),
+    data
 };
